2f2fdd5af364ee58945f80dbddde3421840c101f
LayoutXML <hello@rokasjankunas.com>
2020-02-01 19:12:25 +0000

    Temp graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService} from './services/api.service';
 import {HttpClient} from '@angular/common/http';
 
@@ -7,10 +7,13 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  apiService = new ApiService(this.httpClient);
 
   constructor(private httpClient: HttpClient) {
-    const apiService = new ApiService(httpClient);
-    apiService.fetchLatestBlock();
+  }
+
+  ngOnInit(): void {
+    this.apiService.fetchLatestBlock();
   }
 }
